b465484ba2a3cf8c5c1a82010547fe2097dc733a
GaelGuegan <gaelguegan1995@gmail.com>
2021-05-16 23:20:32 +0200

    Finalize facebook integration

--- js/config.js
+++ js/config.js
@@ -1,19 +1,25 @@
 FBInstant.initializeAsync().then(function() {
 
-    var config = {
-        type: Phaser.AUTO,
-        width: 800,//window.innerWidth,
-        height: 600, //window.innerHeight,
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: { y: 900 },
-                debug: false
-            }
-        },
-        pixelArt: true,
-        scene: [  Game, Pause, GameOver, Preloader ]
-    };
-    var game = new Phaser.Game(config);
+    FBInstant.setLoadingProgress(100);
+
+    FBInstant.startGameAsync().then(function() {
+
+        var config = {
+            type: Phaser.AUTO,
+            width: 800,//window.innerWidth,
+            height: 400, //window.innerHeight,
+            physics: {
+                default: 'arcade',
+                arcade: {
+                    gravity: { y: 900 },
+                    debug: false
+                }
+            },
+            pixelArt: true,
+            scene: [  Game, Pause, GameOver, Preloader ]
+        };
+        var game = new Phaser.Game(config);
+
+    });
 
 });
